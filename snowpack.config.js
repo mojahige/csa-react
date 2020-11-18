bf2cdb466916a6c578a9835d98018b4776c1fcdf
mojahige <mojahige@gmail.com>
2020-11-18 21:41:15 +0900

    add PostCSS

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
+    [
+      '@snowpack/plugin-build-script',
+      { cmd: 'postcss', input: ['.css'], output: ['.css'] },
+    ],
   ],
   install: [
     /* ... */
